cfd1c3e736c0fd23f0fc3b5f7082820c2f377ebd
TechieBott <nehangandhi@live.com>
2025-12-18 19:29:57 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.agentsicworld.com', // Replace with your custom domain
+  outDir: 'dist',
+});
